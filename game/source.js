befb5ebc228d76776c68f3142b2f73f5d0de354e
BWS <tanguo@hotmail.com>
2018-12-24 11:48:50 -0500

    Update source.js
    this cant be the problem.
    like, what the fuck.
    it simply cannot be the entirety of the problem.

--- game/source.js
+++ game/source.js
@@ -51,7 +51,7 @@ window.noname_source_list=[
     'extension/coin/extension.js',
     'extension/wuxing/extension.js',
     'theme/music/grid.png',
-    'theme/spell.gif'
+    'theme/spell.gif',
     'theme/music/style.css',
     'theme/music/wood.png',
     'theme/music/wood3.png',
