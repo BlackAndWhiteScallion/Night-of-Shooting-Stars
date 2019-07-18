205a18d9608a031b2d4fe8dbd848f4712f935c70
BWS <tanguo@hotmail.com>
2019-07-18 17:41:05 -0400

    Update game.js
    right, so what I need to do, is to wait about an hour for the web to properly update.
    
    got it.

--- game/game.js
+++ game/game.js
@@ -23879,13 +23879,15 @@
             ui.window.appendChild(audio);
         },
         playBackgroundMusic:function(){
-            console.log('better work this time');
-            /*
             if (!lib.config.background_music) lib.config.backgroundmusic = 'music_default';
             if(lib.config.background_music=='music_off'){
                 ui.backgroundMusic.src='';
             }
             else{
+                var url = lib.assetURL;
+                if (location.hostname && location.hostname.includes('coding')){
+                    url = 'https://dev.tencent.com/u/BWS/p/Night-of-Shooting-Stars/git/raw/master/';
+                }
                 var music=lib.config.background_music;
                 if(music=='music_random'){
                     music=lib.config.all.background_music.randomGet('music_off','music_random',_status.currentMusic);
@@ -23897,14 +23899,13 @@
                     }
                 }
                 if (music == 'music_default'){
-                    ui.backgroundMusic.src=lib.assetURL+'audio/background/'+music+'.mp3';
+                    ui.backgroundMusic.src=url+'audio/background/'+music+'.mp3';
                     ui.backgroundMusic.currentTime = [137, 693, 1338, 1970, 2715, 3463, 3982].randomGet();
                 }
                 else{
-                    ui.backgroundMusic.src=lib.assetURL+'audio/background/'+music+'.mp3';
+                    ui.backgroundMusic.src=url+'audio/background/'+music+'.mp3';
                 }
             }
-            */
         },
         import:function(type,content){
             if(type=='extension'){
